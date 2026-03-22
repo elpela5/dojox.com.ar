c6ff9457625f6a944b7a6decfcca2ba1ea624292
elpela5 <ianlihuel97@gmail.com>
2026-03-21 21:02:27 -0300

    Delete src/main.tsx

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-// Simple Error Display Component
-import React from 'react';
-
-const SimpleErrorDisplay = ({ message }) => {
-    return <div>{message}</div>;
-};
-
-export default SimpleErrorDisplay;
